b23634d313ede2e0cf17ce60517ac16e547fa057
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-14 19:11:16 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE Students;
+USE Students;
+CREATE TABLE StudentsInfo
+(
+StudentID int,
+StudentName varchar(8000),
+ParentName varchar(8000),
+PhoneNumber bigint,
+AddressofStudent varchar(8000),
+City varchar(8000),
+Country varchar(8000)
+);
+
